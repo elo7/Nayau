d23fb4d07e0f270a7f8203e0e0aad6d7e2f494ff
Abner Terribili <abner.terribili@gmail.com>
2016-04-11 13:14:39 -0300

    Initial Commit

---
+++ nayau/nayau.h
@@ -0,0 +1,19 @@
+//
+//  nayau.h
+//  nayau
+//
+//  Created by Abner Terribili on 4/11/16.
+//  Copyright © 2016 Elo7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for nayau.
+FOUNDATION_EXPORT double nayauVersionNumber;
+
+//! Project version string for nayau.
+FOUNDATION_EXPORT const unsigned char nayauVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nayau/PublicHeader.h>
+
+
